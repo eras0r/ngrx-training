d4a02991d4311d7ba8e6e653de48d36f54b80188
eras0r <er4s0r@gmail.com>
2019-12-19 21:47:28 +0100

    unit tests for todo selector

--- src/app/todos/todos.selectors.spec.ts
+++ src/app/todos/todos.selectors.spec.ts
@@ -1,17 +1,29 @@
 import * as fromTodos from './todos.reducer';
-import {selectTodosState} from './todos.selectors';
+import {selectSelectedTodo, selectTodos, selectTodosState} from './todos.selectors';
 
 describe('Todos Selectors', () => {
-  it('should select the feature state', () => {
-    const todoState = {
-      todos: [],
-      selectedTodo: null
-    };
 
+  const todoState = {
+    todos: [],
+    selectedTodo: null
+  };
+
+  it('should select the feature state', () => {
     const result = selectTodosState({
       [fromTodos.todosFeatureKey]: todoState
     });
 
-    expect(result).toEqual(todoState);
+    expect(result).toBe(todoState);
   });
+
+  it('should select the todos', () => {
+    const result = selectTodos.projector(todoState);
+    expect(result).toBe(todoState.todos);
+  });
+
+  it('should select the selectedTodo', () => {
+    const result = selectSelectedTodo.projector(todoState);
+    expect(result).toBe(todoState.selectedTodo);
+  });
+
 });
