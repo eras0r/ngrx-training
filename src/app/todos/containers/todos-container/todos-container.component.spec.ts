06915f55e3ace1d1802673d73f269b4395ccf6e8
eras0r <er4s0r@gmail.com>
2019-12-19 22:04:34 +0100

    fix tests

--- src/app/todos/containers/todos-container/todos-container.component.spec.ts
+++ src/app/todos/containers/todos-container/todos-container.component.spec.ts
@@ -2,16 +2,37 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TodosContainerComponent} from './todos-container.component';
 import {Store, StoreModule} from '@ngrx/store';
+import {Component} from '@angular/core';
+import {removeCompletedTodos} from '../../todos.actions';
+import {TodosState} from '../../todos.reducer';
+
+@Component({
+  selector: 'app-todos-list-container',
+  template: ''
+})
+export class TodosListContainerStubComponent {
+}
+
+@Component({
+  selector: 'app-todos-edit-container',
+  template: ''
+})
+export class TodosEditContainerComponent {
+}
 
 describe('TodosContainerComponent', () => {
   let component: TodosContainerComponent;
   let fixture: ComponentFixture<TodosContainerComponent>;
-  let store: Store<any>;
+  let store: Store<TodosState>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [StoreModule.forRoot({})],
-      declarations: [TodosContainerComponent]
+      declarations: [
+        TodosContainerComponent,
+        TodosListContainerStubComponent,
+        TodosEditContainerComponent
+      ]
     });
 
     await TestBed.compileComponents();
@@ -20,7 +41,7 @@ describe('TodosContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosContainerComponent);
     component = fixture.componentInstance;
-    store = TestBed.get<Store<any>>(Store);
+    store = TestBed.get<Store<TodosState>>(Store);
 
     spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
@@ -28,5 +49,10 @@ describe('TodosContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch removeCompleted todos when clicking on remove completed todos', () => {
+    component.removeCompletedTodos();
+    expect(store.dispatch).toHaveBeenCalledWith(removeCompletedTodos());
   });
 });
