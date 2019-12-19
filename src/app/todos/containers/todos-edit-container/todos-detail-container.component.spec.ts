9e21185f279f542ff746776ae469ec99783c57b2
eras0r <er4s0r@gmail.com>
2019-12-19 21:40:50 +0100

    fix test for container

--- src/app/todos/containers/todos-edit-container/todos-detail-container.component.spec.ts
+++ src/app/todos/containers/todos-edit-container/todos-detail-container.component.spec.ts
@@ -1,28 +1,61 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TodosEditContainerComponent} from './todos-edit-container.component';
-import {Store, StoreModule} from '@ngrx/store';
+import {MemoizedSelector, Store} from '@ngrx/store';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Todo} from '../../todos.model';
+import {SharedModule} from '../../../shared/shared.module';
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
+import {TodosState} from '../../todos.reducer';
+import {selectSelectedTodo} from '../../todos.selectors';
+
+@Component({
+  selector: 'app-todos-edit',
+  template: ''
+})
+export class TodosEditStubComponent {
+
+  @Input()
+  todo: Todo;
+
+  @Output()
+  todoChanged = new EventEmitter<Todo>();
+
+  @Output()
+  cancelEditTodo = new EventEmitter<void>();
+}
 
 describe('TodosDetailContainerComponent', () => {
   let component: TodosEditContainerComponent;
   let fixture: ComponentFixture<TodosEditContainerComponent>;
-  let store: Store<any>;
+  // let store: Store<any>;
+  let mockStore: MockStore<TodosState>;
+  let mockSelectedTodoSelector: MemoizedSelector<TodosState, Todo>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [StoreModule.forRoot({})],
-      declarations: [TodosEditContainerComponent]
+      imports: [
+        // StoreModule.forRoot({}),
+        SharedModule
+      ],
+      providers: [
+        provideMockStore()
+      ],
+      declarations: [
+        TodosEditContainerComponent,
+        TodosEditStubComponent
+      ]
     });
 
     await TestBed.compileComponents();
+
+    mockStore = TestBed.get(Store);
+    mockSelectedTodoSelector = mockStore.overrideSelector(selectSelectedTodo, null);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosEditContainerComponent);
     component = fixture.componentInstance;
-    store = TestBed.get<Store<any>>(Store);
-
-    spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
   });
 
