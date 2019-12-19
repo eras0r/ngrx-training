47bf70cb99bbeb00a74c116544b6741358db031c
eras0r <er4s0r@gmail.com>
2019-12-19 21:49:19 +0100

    fix tests

--- src/app/todos/components/todos-list-entry/todos-list-entry.component.spec.ts
+++ src/app/todos/components/todos-list-entry/todos-list-entry.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TodosListEntryComponent } from './todos-list-entry.component';
+import {TodosListEntryComponent} from './todos-list-entry.component';
 
 describe('TodosListEntryComponent', () => {
   let component: TodosListEntryComponent;
@@ -8,9 +8,9 @@ describe('TodosListEntryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosListEntryComponent ]
+      declarations: [TodosListEntryComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
